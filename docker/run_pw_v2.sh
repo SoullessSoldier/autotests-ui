b05f3ebe1e02f71e3b9ec79c1cd23031d7533d28
SoullessSoldier <48070769+SoullessSoldier@users.noreply.github.com>
2025-09-16 22:15:48 +0200

    поправил имя контейнера для запуска образа

--- docker/run_pw_v2.sh
+++ docker/run_pw_v2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export MSYS_NO_PATHCONV=1
-docker run --name pw_python_sshd -d -p 2222:22 -v $(dirname $(pwd)):/home/vscode/workspace playwright-python-sshd-v2
+docker run --name pw_python_sshd_v2 -d -p 2222:22 -v $(dirname $(pwd)):/home/vscode/workspace playwright-python-sshd-v2
